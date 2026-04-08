9c50fa845fab15da572893765646a72b70e1a300
zikki <nielswprivat@gmail.com>
2026-04-08 17:03:27 +0200

    Changes by jonas

--- src/main/java/org/example/racekatteklubben/use_case/LogInService.java
+++ src/main/java/org/example/racekatteklubben/use_case/LogInService.java
@@ -1,16 +1,25 @@
 package org.example.racekatteklubben.use_case;
 
+import org.example.racekatteklubben.entity.UserLogin;
 import org.example.racekatteklubben.entity.interfaces.IUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LogInService {
 
-    private IUserRepository userRepository;
-
+    private final IUserRepository userRepository;
     @Autowired
     public LogInService(IUserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public UserLogin login(UserLogin loginRequest) {
+        UserLogin userLogin = userRepository.logUserIn(loginRequest);
+        if (userLogin != null && BCrypt.checkpw(loginRequest.getPassword(), userLogin.getPassword())) {
+            return userLogin;
+        }
+        return null;
     }
 }
