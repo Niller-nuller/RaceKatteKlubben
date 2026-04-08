e89c73d416323e1a9862aa7a7c4c0d88a30e33bd
zikki <nielswprivat@gmail.com>
2026-04-08 17:03:11 +0200

    Merge alle 4 branche sammen.

--- RaceKatteKlubben/src/main/java/org/example/racekatteklubben/entity/Gender.java
+++
@@ -1,6 +0,0 @@
-package org.example.racekatteklubben.entity;
-
-public enum Gender {
-    MALE,
-    FEMALE
-}
--- RaceKatteKlubben/src/main/java/org/example/racekatteklubben/entity/interfaces/ICatRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.racekatteklubben.entity.interfaces;
-
-import org.example.racekatteklubben.entity.Cat;
-
-public interface ICatRepository {
-    void createCat(Cat cat);
-}
