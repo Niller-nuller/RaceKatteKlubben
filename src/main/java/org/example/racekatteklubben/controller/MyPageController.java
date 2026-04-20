2787f2926dbb7c4edff837953884caf8404cecbf
Acefullbuster1234 <Gustavzeus14@gmail.com>
2026-04-20 12:54:27 +0200

    cik

--- src/main/java/org/example/racekatteklubben/controller/MyPageController.java
+++ src/main/java/org/example/racekatteklubben/controller/MyPageController.java
@@ -1,12 +1,19 @@
 package org.example.racekatteklubben.controller;
 
+import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpSession;
-import org.example.racekatteklubben.entity.Auth;
 import org.example.racekatteklubben.entity.User;
 import org.example.racekatteklubben.use_case.CatService;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Controller
 public class MyPageController {
@@ -18,7 +25,7 @@ public class MyPageController {
     }
 
     @GetMapping("/myprofile")
-    public String profile(HttpSession session, Model model){
+    public String profile(HttpSession session, Model model) {
         User user = (User) session.getAttribute("currentUser");
         if (user == null) {
             return "redirect:/login";
@@ -33,5 +40,6 @@ public class MyPageController {
         model.addAttribute("cats", catService.listCatById(userId));
         return "/myprofile";
     }
-
 }
+
+
